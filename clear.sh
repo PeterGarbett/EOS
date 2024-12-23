3d16b93272fd2fa015f1a197c9977e4a8a5a2d80
Peter Garbett <petegarbett1958@gmail.com>
2024-12-23 00:07:12 +0000

    Clear out folders.

--- clear.sh
+++ clear.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #
-
+#exit
 /home/embed/EOS/pwr_on.sh
-
-#Allow time to power up
 sleep 5
-
-# Use example routine to clear down camera
 
 /home/embed/EOS/bin/python3 /home/embed/EOS/lib/python3.11/site-packages/gphoto2/examples/clear-space.py  99
 
+# Need to delete folders as well. If we don't the camera stops working
+# when  folder 999 exists
+
+gphoto2 --delete-all-files --debug --debug-logfile=/home/embed/EOS/delete_log.txt
 
